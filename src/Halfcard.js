d25533fba33a26e86bfcff83355855f187ecc30d
Chethan N <chethann12793@gmail.com>
2017-11-24 15:33:55 +0530

    Added fade animation while opening and closing

--- src/Halfcard.js
+++ src/Halfcard.js
@@ -1,107 +1,154 @@
-'use strict'
 import React, { Component } from 'react'
 import {
 	Animated,
 	Modal,
-	Text,
 	TouchableHighlight,
 	Dimensions,
 	StyleSheet,
+	Keyboard,
 	View } from 'react-native'
-
-class Halfcard extends Component {
-
-	constructor(props) {
-    super(props)
-		let {height} = Dimensions.get('window')
-    this.state = {
-			modalVisible: false,
-			position: new Animated.Value((-1 * height))
-		}
-  }
-
-	render() {
-		let height_style = this.props.height ? {height: this.props.height } : {}
-    return (
-      <View>
-        <Modal
-					transparent={true}
-          visible={this.state.modalVisible}
-					onShow={this.props.onShow}
-					onRequestClose={this.close.bind(this)}>
-
-         <View style={{flex: 1}}>
-				 	<TouchableHighlight
-						style={styles.overlay}
-						underlayColor='rgba(0,0,0,0.4)'
-						onPress = {this.close.bind(this)}>
-							<View/>
-					</TouchableHighlight>
-
-					<Animated.View style = {[styles.content, height_style, {bottom: this.state.position, }]}>
-						{this.props.children}
-					</Animated.View>
-				 </View>
-
-				</Modal>
-      </View>
-    )
-  }
-
-  setModalVisible(visible) {
-    this.setState({modalVisible: visible})
-  }
-
-	animate_up() {
-		let animation_duration = this.props.slideDuration || 300
-		Animated.timing(
-			this.state.position,
-			{
-				toValue: 0,
-				duration: animation_duration
-			},
-		).start()
-	}
-
-	animate_down() {
-		let animation_duration = this.props.slideDuration || 300
-		let {height} = Dimensions.get('window')
-		let slide_down_to = height * -1
-		Animated.timing(
-			this.state.position,
-			{
-				toValue: slide_down_to,
-				duration: animation_duration
-			},
-		).start()
-	}
-
-	close() {
-		let animation_duration = this.props.slideDuration || 300
-		this.animate_down()
-		setTimeout(() => {
-			this.setModalVisible(false)
-		}, animation_duration)
-		this.props.onClose && this.props.onClose()
-	}
-
-	show() {
-		this.setModalVisible(true)
-		this.animate_up()
-	}
-}
 
 const styles = StyleSheet.create({
 	overlay: {
-		flex: 1,
-		backgroundColor: 'rgba(0,0,0,0.4)',
+		flexGrow: 1,
+		backgroundColor: 'rgb(0,0,0)',
 	},
 
 	content: {
 		position: 'absolute',
-		left:0,
-		right:0,
+		left: 0,
+		right: 0,
 	},
+
+	container: { flexGrow: 1 },
 })
+
+class Halfcard extends Component {
+
+	constructor (props) {
+		super(props)
+		const { height } = Dimensions.get('window')
+		this.animateHeight = this.props.height || height
+		this.state = {
+			modalVisible: false,
+			position: new Animated.Value(-1 * this.animateHeight),
+			opacity: new Animated.Value(0),
+		}
+		this.hideModal = this.hideModal.bind(this)
+	}
+
+	componentDidMount () {
+		this.isComponentMounted = true
+	}
+
+	componentWillUnmount () {
+		this.isComponentMounted = false
+	}
+
+	setModalVisible (visible) {
+		this.setState({ modalVisible: visible })
+	}
+
+	animateUp () {
+		const slideAnimation = Animated.timing(
+			this.state.position,
+			{
+				toValue: 0,
+				duration: this.props.slideDuration || 300,
+			},
+		)
+
+		const fadeAnimation = Animated.timing(
+			this.state.opacity,
+			{
+				toValue: 0.4,
+				duration: this.props.slideDuration || 300,
+				useNativeDriver: true,
+			},
+		)
+
+		Animated.parallel([ slideAnimation, fadeAnimation ]).start()
+	}
+
+	animateDown () {
+		const slideDownTo = -1 * this.animateHeight
+		const slideAnimation = Animated.timing(
+			this.state.position,
+			{
+				toValue: this.props.slideDownTo || slideDownTo,
+				duration: this.props.slideDuration || 300,
+			},
+		)
+
+		const fadeAnimation = Animated.timing(
+			this.state.opacity,
+			{
+				toValue: 0,
+				duration: this.props.slideDuration || 300,
+				useNativeDriver: true,
+			},
+		)
+
+		Animated.parallel([ slideAnimation, fadeAnimation ]).start(() => {
+			this.props.onClose && this.props.onClose()
+			this.setModalVisible(false)
+		})
+	}
+
+	close () {
+		if (!this.isComponentMounted)
+			return
+		this.props.onCloseStart && this.props.onCloseStart()
+		this.animateDown()
+		Keyboard.dismiss()
+	}
+
+	show () {
+		if (!this.isComponentMounted)
+			return
+		this.setModalVisible(true)
+		this.animateUp()
+	}
+
+	render () {
+		const heightStyle = this.props.height ? { height: this.props.height } : {}
+		const containerStyle = { opacity: this.state.opacity, flexGrow: 1 }
+		return (
+			<View>
+				<Modal
+					transparent
+					visible={ this.state.modalVisible }
+					onShow={ this.props.onShow }
+					onRequestClose={ this.hideModal }
+				>
+					<View style={ styles.container }>
+						<Animated.View style={ containerStyle }>
+							<TouchableHighlight
+								style={ styles.overlay }
+								onPress={ this.hideModal }
+							>
+								<View />
+							</TouchableHighlight>
+						</Animated.View>
+
+						<Animated.View style={ [ styles.content, heightStyle, { bottom: this.state.position } ] }>
+							{this.props.children}
+						</Animated.View>
+					</View>
+				</Modal>
+			</View>
+		)
+	}
+}
+
+Halfcard.propTypes = {
+	height: React.PropTypes.number,
+	slideDownTo: React.PropTypes.number,
+	onShow: React.PropTypes.func,
+	onClose: React.PropTypes.func,
+	children: React.PropTypes.object,
+	slideDuration: React.PropTypes.number,
+	onCloseStart: React.PropTypes.func,
+}
 
 export default Halfcard
